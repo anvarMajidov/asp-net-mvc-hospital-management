04d664eaba832c20eccab496f4c1086405da908f
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-01-14 19:26:52 +0500

    Add js/css plugins, layout change

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace hospitalservice
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+        {}
     }
 }
