9fea6e21aa931a217151a85cd27b6433d5d04424
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-01-14 18:05:56 +0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HospitalService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
