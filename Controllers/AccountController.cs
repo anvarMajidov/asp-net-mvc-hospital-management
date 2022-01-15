37130868a5e17ba103f2a5c59b784198d7a4342f
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-01-15 07:19:22 +0500

    Added register post method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using hospitalservice.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalService.Models.ViewModels
@@ -6,9 +8,16 @@ namespace HospitalService.Models.ViewModels
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public AccountController(ApplicationDbContext db)
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationUser> _roleManager;
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationUser> roleManager)
         {
             _db = db;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Login()
@@ -17,6 +26,28 @@ namespace HospitalService.Models.ViewModels
         }
         public IActionResult Register()
         {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+                var result = await _userManager.CreateAsync(user);
+                
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent:false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
             return View();
         }
     }
